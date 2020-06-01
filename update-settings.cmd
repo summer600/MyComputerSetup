a42ed6dacd569aff5badcaa045e88bd572642709
Johan Bennink <j.bennink@gameswap.nl>
2020-06-01 13:45:52 +0200

    First set of configs. Not really interesting at the moment but I have to start somewhere. Most other programs don't have a settings export.

---
+++ update-settings.cmd
@@ -0,0 +1,4 @@
+@Echo Off
+copy %USERPROFILE%\AppData\Roaming\code\user\settings.json .\windows\settings\VSCode\
+
+copy %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json .\windows\settings\Windows.Terminal\
